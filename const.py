6dfedb1a26b6c2acbbc7a7bdf0c8d64509341551
jessicsous <jessicasilvasousa.eng@gmail.com>
2025-02-28 23:05:14 -0300

    configurando driver do chrome

--- const.py
+++ const.py
@@ -3,18 +3,25 @@ import os
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import platform
 
-user = os.getenv('USER')
-password = os.getenv('PASS')
-url = 'www.Booking.com.br'
 
 def driver():
-    caminho_driver = os.getcwd() + '/chromedriver'
+    sistema = platform.system()
+
+    if sistema == 'Linux':
+        caminho_driver = os.getcwd() + '/chromedriver/linux/chromedriver'
+    elif sistema == 'Windows':
+        caminho_driver = os.getcwd() + '/chromedriver/windows/chromedriver'    
+    
     service = Service(caminho_driver)
     chrome_options = Options()
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--lang=pt-BR")
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
+    
     return driver
--- env_var.sh
+++
@@ -1,3 +0,0 @@
-export URL='www.Booking.com.br'
-export USER='seu usuário'
-export PASS='sua senha'
